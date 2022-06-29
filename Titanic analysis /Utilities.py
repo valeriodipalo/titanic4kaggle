fd9d25c45f2250e0054b35ec94b6a592e2b5c979
valeriodipalo <82063143+valeriodipalo@users.noreply.github.com>
2022-06-28 17:34:23 -0700

    Divided the single notebook in different ones for clarity

---
+++ Titanic analysis /Utilities.py
@@ -0,0 +1,9 @@
+def importer():
+    import pandas as pd
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from IPython.display import Image, display
+
+
+
